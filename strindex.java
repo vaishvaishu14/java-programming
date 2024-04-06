c1da9dfa245c9e1bdc4f5eec69ceb913f25f1b1c
vaishvaishu14 <vaishnaviharish14@gmail.com>
2024-04-06 15:08:17 +0530

    implemented program

---
+++ strindex.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class strindex
+{
+public static void main(String args[])
+{
+String txt="where is the location of your college?";
+System.out.println(txt.indexOf("location"));
+}
+}
